e2eade173e9e78e153fe706699f46b90baf045e0
JonCabot <jfc0053@gmail.com>
2022-05-31 14:39:23 -0500

    commit tree 1baa78ced3b94a66601a5f03ca2a564f528a601d

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,47 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+  # GET all
   def index
-    items = Item.all
+    if params[:user_id]
+      user = find_user
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  # GET show only based on :id
+  def show
+    item = find_item
+    render json: item
+  end
+  
+  # POST new item; grab userId, set item to nested params
+  def create 
+    user = find_user
+    item = user.items.create(item_params)
+    render json: item, status: :created
+  end
+
+
+  private
+
+  def find_item
+    Item.find(params[:id])
+  end
+
+  def find_user
+    User.find(params[:user_id])
+  end
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def render_not_found_response(exception)
+    render json: { error: "#{exception.model} not found" }, status: :not_found
   end
 
 end
